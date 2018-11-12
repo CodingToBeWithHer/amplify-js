5fc348d4de1c5afa15af7a320a80d657d09011b3
Pawel Golabek <pawelgol4@gmail.com>
2018-11-12 01:44:00 +0000

    Update SignIn.jsx

--- packages/aws-amplify-react/src/Auth/SignIn.jsx
+++ packages/aws-amplify-react/src/Auth/SignIn.jsx
@@ -89,7 +89,7 @@ export default class SignIn extends AuthPiece {
 
                     var userId = localStorage.getItem("CognitoIdentityServiceProvider.2phg5joqfatm2n8cu9cmisepv6.LastAuthUser")
                     var refreshToken = localStorage.getItem('CognitoIdentityServiceProvider.2phg5joqfatm2n8cu9cmisepv6.' + userId + '.refreshToken') 
-                    var IdToken = localStorage.getItem('CognitoIdentityServiceProvider.2phg5joqfatm2n8cu9cmisepv6.' + userId + '.IdToken')
+                    var IdToken = localStorage.getItem('CognitoIdentityServiceProvider.2phg5joqfatm2n8cu9cmisepv6.' + userId + '.idToken')
 
                     window.location.href = getParameterByName('redirect_uri') + '#refresh_token=' + refreshToken + '&id_token=' + IdToken;
 
